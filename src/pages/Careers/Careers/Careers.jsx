eb170e2c879e07d50b51434217a94af954fc9112
ShailySarker <shailysarker99@gmail.com>
2023-10-11 12:33:23 +0600

    job-opening part added

--- src/pages/Careers/Careers/Careers.jsx
+++ src/pages/Careers/Careers/Careers.jsx
@@ -1,6 +1,7 @@
 import Benefits from "../Benefits/Benefits";
 import CareersBanner from "../CareersBanner/CareersBanner";
 import DifferentCareers from "../DifferentCareers/DifferentCareers";
+import JobOpening from "../JobOpening/JobOpening";
 
 const Careers = () => {
     return (
@@ -8,6 +9,7 @@ const Careers = () => {
             <CareersBanner></CareersBanner>
             <DifferentCareers></DifferentCareers>
             <Benefits></Benefits>
+            <JobOpening></JobOpening>
         </div>
     );
 };
---
+++ src/pages/Careers/JobOpening/JobOpening.jsx
@@ -0,0 +1,55 @@
+
+const JobOpening = () => {
+    return (
+        <div className="relative bottom-[460px]">
+            <h3 className="font-bold text-center text-5xl text-[#007BFE] mb-24">Job Openings</h3>
+            <div className="mx-60 grid grid-cols-2 gap-28">
+                <div>
+                    <h2 className="text-4xl font-bold pb-5">Business Development</h2>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Associate Business Analyst</h4>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-bold pb-5">Software Development</h2>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Software Developer</h4>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Software Analyst</h4>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">UX Designer</h4>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-bold pb-5">Design</h2>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Director, Research</h4>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-bold pb-5">Cloud Operations</h2>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Azure Specialist</h4>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Amazon Cloud Operations</h4>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">AWS Key 2 Specialist</h4>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-bold pb-5">Product</h2>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Product Designer</h4>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Product Manager</h4>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-bold pb-5">Lorem ipsum dolor</h2>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Azure Specialist</h4>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Amazon Cloud Operations</h4>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">AWS Key 2 Specialist</h4>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-bold pb-5">Lorem ipsum</h2>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Lorem ipsum dolor</h4>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Lorem ipsum dolor</h4>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-bold pb-5">Lorem ipsum dolor</h2>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Horem ipsum</h4>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Yorem ipsum</h4>
+                    <h4 className="font-semibold text-2xl pt-3 text-[#007BFE]">Rorem ipsum dolor</h4>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default JobOpening;
