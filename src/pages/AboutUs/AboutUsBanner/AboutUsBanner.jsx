178c491596af836f86a6ee9b00187494e3dbf6d8
ShailySarker <shailysarker99@gmail.com>
2023-10-23 13:29:28 +0600

    made aboutUs_banner section responsive

--- src/pages/AboutUs/AboutUsBanner/AboutUsBanner.jsx
+++ src/pages/AboutUs/AboutUsBanner/AboutUsBanner.jsx
@@ -2,19 +2,19 @@
 const AboutUsBanner = () => {
     return (
         <div className="my-12">
-            <h2 className="font-bold text-5xl text-center mb-36">About Us</h2>
-            <div className="grid grid-cols-2 gap-36 items-center">
-                <div className="ml-28">
-                    <h2 className="font-bold text-5xl pb-10">SEO Impact</h2>
-                    <h4 className="text-2xl font-semibold text-slate-400 pb-20 pr-28">Gorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</h4>
-                    <button className="text-white bg-[#007BFE] py-3 px-6 rounded-xl font-semibold">Learn More</button>
+            <h2 className="font-bold text-5xl text-center lg:mb-36 mb-24 ">About Us</h2>
+            <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-36 gap-24 items-center lg:mx-0 md:mx-16 mx-6 mb-10">
+                <div className="lg:ml-28">
+                    <h2 className="font-bold md:text-5xl text-3xl pb-10">SEO Impact</h2>
+                    <h4 className="md:text-2xl text-lg font-semibold text-slate-400 md:pb-20 pb-16 lg:pr-28">Gorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</h4>
+                    <button className="text-white bg-[#007BFE] md:py-3 md:px-6 p-2 w-28 md:w-36 rounded-xl font-semibold">Learn More</button>
 
                 </div>
-                <div>
+                <div className="md:px-12 lg:px-0">
                     <img src="https://i.ibb.co/YNmfjxB/pana.png" alt="" />
                 </div>
             </div>
-            <div className=" my-36">
+            <div className=" md:py-40 py-20">
                 <img src="https://i.ibb.co/d4WCd6z/Vector-10.png" alt="" />
             </div>
         </div>
