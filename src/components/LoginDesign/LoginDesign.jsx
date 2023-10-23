47d9a88cc98340e2a98d5a0147a94935f221a914
ShailySarker <shailysarker99@gmail.com>
2023-10-23 15:07:56 +0600

    made login page responsive

--- src/components/LoginDesign/LoginDesign.jsx
+++ src/components/LoginDesign/LoginDesign.jsx
@@ -3,10 +3,10 @@ const LoginDesign = () => {
     return (
         <div>
             <div>
-                <img className="w-[850px] h-[740px]" src="https://i.ibb.co/JBNqyPz/Ellipse-50.png" alt="" />
+                <img className="md:w-[850px] w-[300px] md:h-[740px] h-[600px]" src="https://i.ibb.co/JBNqyPz/Ellipse-50.png" alt="" />
             </div>
-            <div className="relative bottom-[600px] -mb-[600px] left-96 mr-96">
-                <img className="w-64 h-[600px] " src="https://i.ibb.co/RyFppy8/Group-175.png" alt="" />
+            <div className="relative md:bottom-[600px] bottom-[500px] md:-mb-[600px] -mb-[400px] lg:left-96 md:left-80 left-24 md:mr-96 mr-24">
+                <img className="md:w-64 w-32 md:h-[600px] h-[500px] " src="https://i.ibb.co/RyFppy8/Group-175.png" alt="" />
             </div>
         </div>
     );
--- src/pages/RegisterLogin/Login/Login.jsx
+++ src/pages/RegisterLogin/Login/Login.jsx
@@ -3,7 +3,7 @@ import LoginDesign from "../../../components/LoginDesign/LoginDesign";
 
 const Login = () => {
     return (
-        <div className="mt-12 ml-36">
+        <div className="mt-12 lg:ml-36 md:ml-16 ml-6 ">
             <div className="flex justify-between">
                 <div>
                     <img src="https://i.ibb.co/LgjTcLv/Group-157.png" alt="" />
@@ -11,9 +11,9 @@ const Login = () => {
                 <LoginDesign></LoginDesign>
             </div>
             <div className="">
-                <div className="mx-auto flex relative -top-[150px] -mt-[470px]">
+                <div className="mx-auto flex relative -top-[150px] md:-mt-[470px] -mt-[450px]">
                     <div>
-                        <h2 className="font-bold text-5xl mb-24">Login</h2>
+                        <h2 className="font-bold text-5xl md:mb-24 mb-14">Login</h2>
                         <form>
                             <div className="form-control my-5">
                                 <input
@@ -34,20 +34,20 @@ const Login = () => {
                             </div>
 
                             <div className="text-right font-bold">
-                                <Link className="text-[#007BFE]">Forget your password?</Link>
+                                <Link className="lg:text-[#007BFE] md:text-white text-slate-600">Forget your password?</Link>
                             </div>
 
                             <input
                                 type="submit"
                                 value="Login"
-                                className="text-white bg-[#007BFE] py-3 px-6 rounded-xl font-semibold mt-10 mx-28 w-40"
+                                className="text-white bg-[#007BFE] py-3 px-6 rounded-xl font-semibold mt-10 lg:mx-28 md:mr-72  lg:w-40 w-32"
                             />
 
                         </form>
 
-                        <div className="text-center mt-10">
-                            <p className="text-slate-400">
-                                Don’t have an account? <Link to='/signUp' className="font-bold text-[#007BFE]">Signup</Link>
+                        <div className="lg:text-center mt-10">
+                            <p className="lg:text-slate-400 text-black   ">
+                                Don’t have an account?  <Link to='/signUp' className="font-bold md:text-white lg:text-[#007BFE] text-slate-600"> Signup</Link>
                             </p>
                         </div>
                     </div>
