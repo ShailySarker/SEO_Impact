000a71fbff67519079c3b52710bc47a2f11b39ee
ShailySarker <shailysarker99@gmail.com>
2023-10-24 19:51:59 +0600

    made resources_backlinkCheckerResulting_backlinkResultOverview section responsive

--- src/pages/Resources/BackLinkChecker/BackLinkCheckerAfterCheck/BackLinkCheckerAfterCheck.jsx
+++ src/pages/Resources/BackLinkChecker/BackLinkCheckerAfterCheck/BackLinkCheckerAfterCheck.jsx
@@ -3,13 +3,13 @@ import BackLinkResultOverview from "../BackLinkResultOverview/BackLinkResultOver
 
 const BackLinkCheckerAfterCheck = () => {
     return (
-        <div className="mt-12 ml-16">
-            <h2 className="font-semibold text-5xl">Backlink Checker</h2>
+        <div className="mt-12 lg:ml-16 md:ml-14 md:mr-0 mx-6">
+            <h2 className="font-semibold lg:text-start text-center lg:text-5xl md:text-4xl text-3xl">Backlink Checker</h2>
             <div className="mt-24 flex flex-col items-center gap-10">
-                <input className="border-2 py-3 px-7 w-96 rounded-lg" type="search" name="" id="" placeholder="cbb.coastalbroadband.in" disabled/>
+                <input className="border-2 py-3 px-7 md:w-96 w-72 rounded-lg" type="search" name="" id="" placeholder="cbb.coastalbroadband.in" disabled/>
             </div>
             <BackLinkResultOverview></BackLinkResultOverview>
-            <BackLinkResultDescription></BackLinkResultDescription>
+            {/* <BackLinkResultDescription></BackLinkResultDescription> */}
         </div>
     );
 };
--- src/pages/Resources/BackLinkChecker/BackLinkResultOverview/BackLinkResultOverview.jsx
+++ src/pages/Resources/BackLinkChecker/BackLinkResultOverview/BackLinkResultOverview.jsx
@@ -1,52 +1,52 @@
 
 const BackLinkResultOverview = () => {
     return (
-        <div className="mt-28 mr-44">
-            <div className="grid grid-cols-3  gap-10">
-                <div className="bg-[#379FFF1A] py-6 px-7 rounded-xl">
+        <div className="mt-28 md:mx-0 mx-12">
+            <div className="flex md:flex-row flex-col gap-10">
+                <div className="bg-[#379FFF1A] px-3 py-6 rounded-xl lg:w-[250px] md:w-52 lg:h-40 md:h-44">
                     <div className="flex gap-6 items-center ">
                         <img src="https://i.ibb.co/ZLDGZ6q/mdi-world.png" alt="" />
-                        <h3 className=" text-2xl font-semibold">Domain Rating</h3>
+                        <h3 className=" lg:text-2xl md:text-xl text-lg font-semibold">Domain Rating</h3>
                     </div>
                     <div>
-                        <h2 className="text-4xl font-bold text-center pt-8">5</h2>
+                        <h2 className="lg:text-4xl md:text-3xl text-2xl font-bold text-center pt-8">5</h2>
                     </div>
                 </div>
-                <div className="bg-[#379FFF1A] py-5 px-7 rounded-xl">
+                <div className="bg-[#379FFF1A] px-3 py-6 rounded-xl lg:w-[250px] md:w-52 lg:h-40 md:h-44">
                     <div className="flex gap-6 items-center ">
                         <img src="https://i.ibb.co/ZLDGZ6q/mdi-world.png" alt="" />
-                        <h3 className=" text-2xl font-semibold">Domains</h3>
+                        <h3 className=" lg:text-2xl md:text-xl text-lg font-semibold">Domains</h3>
                     </div>
                     <div>
-                        <h2 className="text-4xl font-bold text-center pt-8">1</h2>
+                        <h2 className="lg:text-4xl md:text-3xl text-2xl font-bold text-center pt-8">1</h2>
                     </div>
                 </div>
-                <div className="bg-[#379FFF1A] py-5 px-7 rounded-xl">
+                <div className="bg-[#379FFF1A] px-3 py-6 rounded-xl lg:w-[250px] md:w-52 lg:h-40 md:h-44">
                     <div className="flex gap-6 items-center ">
                         <img src="https://i.ibb.co/x8k7prj/solar-link-bold.png" alt="" />
-                        <h3 className=" text-2xl font-semibold">Backlinks</h3>
+                        <h3 className=" lg:text-2xl md:text-xl text-lg font-semibold">Backlinks</h3>
                     </div>
                     <div>
-                        <h2 className="text-4xl font-bold text-center pt-8">7</h2>
+                        <h2 className="lg:text-4xl md:text-3xl text-2xl font-bold text-center pt-8">7</h2>
                     </div>
                 </div>
             </div>
             <div>
-                <div className="grid grid-cols-2 justify-center mt-16 mx-40 gap-10">
-                    <div className="bg-[#379FFF1A] py-6 px-7 rounded-xl">
+                <div className="flex md:flex-row flex-col md:mt-16 mt-10 lg:mx-32 md:mx-[120px] gap-10">
+                    <div className="bg-[#379FFF1A] px-3 py-6 rounded-xl lg:w-[250px] md:w-52 lg:h-40 md:h-44">
                         <div className="text-center">
-                            <h3 className=" text-2xl font-semibold">Do Follow</h3>
+                            <h3 className=" lg:text-2xl md:text-xl text-lg font-semibold">Do Follow</h3>
                         </div>
                         <div>
-                            <h2 className="text-4xl font-bold text-center pt-8">7</h2>
+                            <h2 className="lg:text-4xl md:text-3xl text-2xl font-bold text-center pt-8">7</h2>
                         </div>
                     </div>
-                    <div className="bg-[#379FFF1A] py-5 px-7 rounded-xl">
+                    <div className="bg-[#379FFF1A] px-3 py-6 rounded-xl lg:w-[250px] md:w-52 lg:h-40 md:h-44">
                         <div className="text-center">
-                            <h3 className=" text-2xl font-semibold">No Follow</h3>
+                            <h3 className=" lg:text-2xl md:text-xl text-lg font-semibold">No Follow</h3>
                         </div>
                         <div>
-                            <h2 className="text-4xl font-bold text-center pt-8">0</h2>
+                            <h2 className="lg:text-4xl md:text-3xl text-2xl font-bold text-center pt-8">0</h2>
                         </div>
                     </div>
                 </div>
