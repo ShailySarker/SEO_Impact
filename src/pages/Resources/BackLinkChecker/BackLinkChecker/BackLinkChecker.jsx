2adc5288d0d7b3349c7bd3b45746476344abf7e6
ShailySarker <shailysarker99@gmail.com>
2023-10-24 18:57:17 +0600

    made resources_backlinkChecker page responsive

--- src/pages/Resources/BackLinkChecker/BackLinkChecker/BackLinkChecker.jsx
+++ src/pages/Resources/BackLinkChecker/BackLinkChecker/BackLinkChecker.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const BackLinkChecker = () => {
     return (
-        <div className="mt-12 ml-16">
-            <h2 className="font-semibold text-5xl">Backlink Checker</h2>
-            <div className="mt-24 flex flex-col items-center gap-10">
-                <input className="border-2 py-3 px-7 w-96 rounded-lg" type="search" name="" id="" placeholder="Enter your website...." />
+        <div className="mt-12 md:ml-16">
+            <h2 className="font-semibold lg:text-5xl md:text-4xl text-3xl lg:text-start text-center">Backlink Checker</h2>
+            <div className="md:mt-24 mt-20 flex flex-col items-center gap-10">
+                <input className="border-2 py-3 px-7 md:w-96 w-72  rounded-lg" type="search" name="" id="" placeholder="Enter your website...." />
                 <Link to="/resources/backLinkCheckerAfterChecking">
-                    <button className="text-white bg-[#007BFE] py-3 px-6 rounded-xl w-36 font-semibold mt-8">Check</button>
+                    <button className="text-white bg-[#007BFE] md:py-3 md:px-6 p-2 rounded-xl w-36 font-semibold mt-8">Check</button>
                 </Link>
             </div>
         </div>
