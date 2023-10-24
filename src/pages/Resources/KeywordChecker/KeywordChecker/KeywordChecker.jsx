e84d2786939cd57e66d80e733c22e416dc028457
ShailySarker <shailysarker99@gmail.com>
2023-10-24 14:42:47 +0600

    made resources_keywordChecker page responsive

--- src/pages/Resources/KeywordChecker/KeywordChecker/KeywordChecker.jsx
+++ src/pages/Resources/KeywordChecker/KeywordChecker/KeywordChecker.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const KeywordChecker = () => {
     return (
-        <div className="mt-12 ml-16">
-            <h2 className="font-semibold text-5xl">Keyword Checker</h2>
-            <div className="mt-24 flex flex-col items-center gap-10">
-                <input className="border-2 py-3 px-7 w-96 rounded-lg" type="search" name="" id="" placeholder="Enter Keyword" />
-                <input className="border-2 py-3 px-7 w-96 rounded-lg" type="search" name="" id="" placeholder="Enter your website...." />
+        <div className="mt-12 md:ml-16">
+            <h2 className="font-semibold md:text-5xl text-3xl md:text-start text-center">Keyword Checker</h2>
+            <div className="md:mt-24 mt-20 flex flex-col items-center md:gap-10 gap-5">
+                <input className="border-2 py-3 px-7 md:w-96 w-72 rounded-lg" type="search" name="" id="" placeholder="Enter Keyword" />
+                <input className="border-2 py-3 px-7 md:w-96 w-72 rounded-lg" type="search" name="" id="" placeholder="Enter your website...." />
                 <Link to="/resources/keywordCheckerAfterChecking">
-                    <button className="text-white bg-[#007BFE] py-3 px-6 rounded-xl w-36 font-semibold mt-8">Check</button>
+                    <button className="text-white bg-[#007BFE] md:py-3 md:px-6 p-2 rounded-xl w-36 font-semibold mt-8">Check</button>
                 </Link>
             </div>
         </div>
