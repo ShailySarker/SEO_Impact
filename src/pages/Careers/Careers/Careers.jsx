2f61c729aa39d89f3cfed0b75eee6838a08d7aee
ShailySarker <shailysarker99@gmail.com>
2023-10-29 00:26:14 +0600

    careers_employeeTestimonials section added with responsive design

--- src/pages/Careers/Careers/Careers.jsx
+++ src/pages/Careers/Careers/Careers.jsx
@@ -3,6 +3,7 @@ import Benefits from "../Benefits/Benefits";
 import CareersBanner from "../CareersBanner/CareersBanner";
 import ContactRecruiter from "../ContactRecruiter/ContactRecruiter";
 import DifferentCareers from "../DifferentCareers/DifferentCareers";
+import EmployeeTestimonials from "../EmployeeTestimonials/EmployeeTestimonials";
 import InternshipOpportunities from "../InternshipOpportunities/InternshipOpportunities";
 import JobOpening from "../JobOpening/JobOpening";
 import SEOImpactRating from "../SEOImpactRating/SEOImpactRating";
@@ -17,6 +18,7 @@ const Careers = () => {
             <InternshipOpportunities></InternshipOpportunities>
             <ApplicationProcess></ApplicationProcess>
             <ContactRecruiter></ContactRecruiter>
+            <EmployeeTestimonials></EmployeeTestimonials>
             <SEOImpactRating></SEOImpactRating>
         </div>
     );
