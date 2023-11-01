9078c3c595a03f9cd16dcb8af589064b499c43c3
ShailySarker <shailysarker99@gmail.com>
2023-11-01 19:26:19 +0600

    fix4_purpose section made the hover animation smooth

--- src/components/PurposeBox/PurposeBox.jsx
+++ src/components/PurposeBox/PurposeBox.jsx
@@ -6,16 +6,16 @@ const PurposeBox = () => {
             </div>
 
             <div className="grid grid-cols-2 md:gap-16 gap-10 relative md:bottom-80 bottom-[200px] ">
-                <div className="bg-[#EBF5FF] hover:bg-[#007BFE] hover:text-white rounded-xl md:h-48 flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE]">
+                <div className="bg-[#EBF5FF] rounded-xl md:h-48 flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE] transition duration-300 ease-in-out hover:bg-[#007BFE] hover:text-white">
                     <h3 className="md:text-3xl text-xl font-semibold p-5 ">Easily Usable </h3>
                 </div>
-                <div className="bg-[#EBF5FF] hover:bg-[#007BFE] hover:text-white rounded-xl md:h-48 flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE]">
+                <div className="bg-[#EBF5FF] rounded-xl md:h-48 flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE] transition duration-300 ease-in-out hover:bg-[#007BFE] hover:text-white">
                     <h3 className="md:text-3xl text-xl font-semibold p-5 ">Quick Service</h3>
                 </div>
-                <div className="bg-[#EBF5FF] hover:bg-[#007BFE] hover:text-white rounded-xl md:h-48 flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE]">
+                <div className="bg-[#EBF5FF] rounded-xl md:h-48 flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE] transition duration-300 ease-in-out hover:bg-[#007BFE] hover:text-white">
                     <h3 className="md:text-3xl text-xl font-semibold p-5 ">Trustable </h3>
                 </div>
-                <div className="bg-[#EBF5FF] hover:bg-[#007BFE] hover:text-white rounded-xl md:h-48 flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE]">
+                <div className="bg-[#EBF5FF] rounded-xl md:h-48 flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE] transition duration-300 ease-in-out hover:bg-[#007BFE] hover:text-white">
                     <h3 className="md:text-3xl text-xl font-semibold p-5 ">Better Result </h3>
                 </div>
             </div>
