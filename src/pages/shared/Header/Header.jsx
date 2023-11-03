f81e8d6cdd7cea333ee089f3a3b05da02139f751
ShailySarker <shailysarker99@gmail.com>
2023-11-03 16:59:33 +0600

    shared_header part updated

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import NavMenu from "../NavMenu/NavMenu";
+import logo from '../../../assets/images/Shared/Header/logo.png';
 
 const Header = () => {
     return (
@@ -9,7 +10,7 @@ const Header = () => {
                 <div className="navbar-start">
                     <div className="flex lg:flex-col flex-row 
                     justify-center items-center lg:ml-0">
-                        <img className="lg:border-r-2 lg:pr-24" src='https://i.ibb.co/LgjTcLv/Group-157.png' alt="" />
+                        <img className="lg:border-r-2 lg:pr-24" src={logo} alt="logo" />
                     </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
@@ -20,7 +21,7 @@ const Header = () => {
                 <div className="navbar-end">
                     <div className="lg:flex lg:visible hidden">
                         <Link to='/login'><button className="py-2 px-3 rounded-xl text-white w-20 bg-[#007BFE] font-semibold mr-6">Login</button></Link>
-                        <Link to='/signUp'><button className=" py-2 px-3 rounded-xl border-2 font-semibold w-20">Signup</button></Link>
+                        <Link to='/signUp'><button className=" py-2 px-3 rounded-xl border-2 font-semibold w-20">SignUp</button></Link>
                     </div>
                     <div className="dropdown">
                         <div className="drawer drawer-end">
@@ -36,12 +37,11 @@ const Header = () => {
                                 <ul className="menu py-6 px-4 md:px-6 md:mt-20 mt-16 w-40 md:w-52 rounded-l-lg bg-base-200 text-base-content">
                                     {/* Sidebar content here */}
                                     <NavMenu></NavMenu>
-                                    {/* <li><Link to='/signUp' className=" rounded-3xl bg-[#0030FF] text-white font-bold">Sign Up</Link></li> */}
                                     <li>
                                         <Link to='/login'><button className="py-2 px-3 rounded-xl text-white w-20 bg-[#007BFE] font-semibold">Login</button></Link>
                                     </li>
                                     <li>
-                                        <Link to='/signUp'><button className=" py-2 px-3 rounded-xl border-2 font-semibold w-20">Signup</button></Link>
+                                        <Link to='/signUp'><button className=" py-2 px-3 rounded-xl border-2 font-semibold w-20">SignUp</button></Link>
                                     </li>
                                 </ul>
                             </div>
