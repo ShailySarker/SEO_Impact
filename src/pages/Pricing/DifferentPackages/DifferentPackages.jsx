9127a7528dc1a68dfa0f16bee912e9c698e430e3
ShailySarker <shailysarker99@gmail.com>
2023-11-03 17:55:51 +0600

    pricing_differentPackages section updated

--- src/pages/Pricing/DifferentPackages/DifferentPackages.jsx
+++ src/pages/Pricing/DifferentPackages/DifferentPackages.jsx
@@ -1,3 +1,8 @@
+import logo from "../../../assets/images/Pricing/DifferentPackages/logo.png";
+import Basic from "../../../assets/images/Pricing/DifferentPackages/Basic.png";
+import Premium from "../../../assets/images/Pricing/DifferentPackages/Premium.png";
+import Enterprise from "../../../assets/images/Pricing/DifferentPackages/Enterprise.png";
+
 
 const DifferentPackages = () => {
     return (
@@ -5,7 +10,7 @@ const DifferentPackages = () => {
             <div className="pb-6">
                 <div className="border-2 p-5 rounded-3xl">
                     <div className="flex flex-col text-center border-b-2">
-                        <img className="mx-auto pt-4" src="https://i.ibb.co/tpxtkdM/Group-25.png" alt="" />
+                        <img className="mx-auto pt-4" src={Basic} alt="Basic" />
                         <h2 className="font-bold lg:text-5xl md:text-4xl text-3xl mt-5 mb-2 text-[#007BFE]">$350.00</h2>
                         <h4 className="text-[#DEDEDE] md:text-xl text-lg font-bold pb-10"><span className="line-through">$450.00</span> | 35% OFF</h4>
                     </div>
@@ -14,19 +19,19 @@ const DifferentPackages = () => {
                         <h3 className="text-lg font-bold  text-[#007BFE] pt-3 pb-8">Top Features :</h3>
                         <div className="flex flex-col gap-3 mb-7">
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">1 Project to optimize your website</h3>
                             </div>
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">Track 50 keywords</h3>
                             </div>
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">Analyze upto 2500 pages</h3>
                             </div>
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">Unlimited PDF Reports</h3>
                             </div>
                         </div>
@@ -40,7 +45,7 @@ const DifferentPackages = () => {
             <div className="pb-6">
                 <div className="border-2 p-5 rounded-3xl">
                     <div className="flex flex-col text-center border-b-2">
-                        <img className="mx-auto pt-4" src="https://i.ibb.co/CW0J2Zg/Group-25-1.png" alt="" />
+                        <img className="mx-auto pt-4" src={Premium} alt="Premium" />
                         <h2 className="font-bold lg:text-5xl md:text-4xl text-3xl mt-5 mb-2 text-[#007BFE]">$850.00</h2>
                         <h4 className="text-[#DEDEDE] md:text-xl text-lg font-bold pb-10"><span className="line-through">$1050.00</span> | 35% OFF</h4>
                     </div>
@@ -49,19 +54,19 @@ const DifferentPackages = () => {
                         <h3 className="text-lg font-bold  text-[#007BFE] pt-3 pb-8">Top Features :</h3>
                         <div className="flex flex-col gap-3 mb-7">
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">3 Project to optimize your website</h3>
                             </div>
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">Track 200 keywords</h3>
                             </div>
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">Analyze upto 2500 pages</h3>
                             </div>
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">Unlimited PDF Reports</h3>
                             </div>
                         </div>
@@ -75,7 +80,7 @@ const DifferentPackages = () => {
             <div className="pb-6">
                 <div className="border-2 p-5 rounded-3xl">
                     <div className="flex flex-col text-center border-b-2">
-                        <img className="mx-auto pt-4" src="https://i.ibb.co/XtWq9sW/Group-25-2.png" alt="" />
+                        <img className="mx-auto pt-4" src={Enterprise} alt="Enterprise" />
                         <h2 className="font-bold lg:text-5xl md:text-4xl text-3xl mt-5 mb-2 text-[#007BFE]">$2050.00</h2>
                         <h4 className="text-[#DEDEDE] md:text-xl text-lg font-bold pb-10"><span className="line-through">$2550.00</span> | 35% OFF</h4>
                     </div>
@@ -84,19 +89,19 @@ const DifferentPackages = () => {
                         <h3 className="text-lg font-bold  text-[#007BFE] pt-3 pb-8">Top Features :</h3>
                         <div className="flex flex-col gap-3 mb-7">
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">5 Project to optimize your website</h3>
                             </div>
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">Track 5000 keywords</h3>
                             </div>
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">Analyze upto 2500 pages</h3>
                             </div>
                             <div className="flex gap-5 items-center">
-                                <img src="https://i.ibb.co/N9PZBhX/Group-27.png" alt="" />
+                                <img src={logo} alt="logo" />
                                 <h3 className="text-lg">Unlimited PDF Reports</h3>
                             </div>
                         </div>
