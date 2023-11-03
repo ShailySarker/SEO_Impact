5a0ca8ef80f81f067468612bb1373690e197dae7
ShailySarker <shailysarker99@gmail.com>
2023-11-03 17:41:01 +0600

    pricing_pricingDesign section updated

--- src/pages/Pricing/PricingDesign/PricingDesign.jsx
+++ src/pages/Pricing/PricingDesign/PricingDesign.jsx
@@ -1,8 +1,10 @@
+import design from '../../../assets/images/Pricing/PricingDesign/design.png';
+
 
 const PricingDesign = () => {
     return (
         <div className="my-24 ">
-            <img className="w-screen" src="https://i.ibb.co/xjjmczx/image.png" alt="" />
+            <img className="w-screen" src={design} alt="" />
         </div>
     );
 };
