4d195fd0f195d4cad5b6e1f3ed9d14dcf29d1b0d
ShailySarker <shailysarker99@gmail.com>
2023-11-03 16:48:56 +0600

    home_MarketInsights section updated

--- src/pages/Home/MarketInsights/MarketInsights.jsx
+++ src/pages/Home/MarketInsights/MarketInsights.jsx
@@ -1,3 +1,5 @@
+import photo from '../../../assets/images/Home/MarketInsights/Rectangle.png';
+
 
 const MarketInsights = () => {
     return (
@@ -5,10 +7,7 @@ const MarketInsights = () => {
             <div>
                 <h2 className="lg:text-5xl md:text-4xl text-3xl font-bold mb-10 text-[#212121]">Increase your <br />Market Insights</h2>
                 <p className="lg:text-2xl md:text-xl text-lg text-[#757575] font-semibold pr-6 md:pr-40">Lorem ipsum dolor sit amet consectetur. Quis eget vivamus neque velit enim id urna pharetra vel. Sapien ut natoque facilisi tortor viverra eget. Amet rhoncus leo sed nulla est senectus morbi tellus ornare. Justo curabitur iaculis id senectus sodales mattis sed mattis nisl. Phasellus ipsum ultricies.</p>
-                {/* <p className="md:text-2xl text-[#757575] font-semibold pr-32 md;pr-0">Lorem ipsum dolor sit amet consectetur. Quis eget <br />vivamus neque velit enim id urna pharetra vel. <br />Sapien ut natoque facilisi tortor viverra eget. Amet <br />rhoncus leo sed nulla est senectus morbi tellus <br />ornare. Justo curabitur iaculis id senectus sodales <br />mattis sed mattis nisl. Phasellus ipsum ultricies.</p>
- */}
                 <button className="text-white bg-[#007BFE] md:py-3 md:px-6 p-2 w-36 rounded-xl font-semibold mt-16 text-lg">Learn More</button>
-
             </div>
             <div className="flex justify-end">
                 <div className="relative -top-16 md:-right-20 -right-12">
@@ -36,7 +35,7 @@ const MarketInsights = () => {
                     </div>
                 </div>
                 <div className="">
-                    <img className="lg:h-[640px] md:h-[600px] h-[480px]" src="https://i.ibb.co/1vSs8Cj/Rectangle-21.png" alt="" />
+                    <img className="lg:h-[640px] md:h-[600px] h-[480px]" src={photo} alt="bg photo" />
                 </div>
 
             </div>
