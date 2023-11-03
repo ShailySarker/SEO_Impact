ae551c19c57860e6d12249c94704f1d810adb801
ShailySarker <shailysarker99@gmail.com>
2023-11-03 18:08:49 +0600

    pricing_buying section updated

--- src/pages/Pricing/Buying/Buying.jsx
+++ src/pages/Pricing/Buying/Buying.jsx
@@ -1,4 +1,4 @@
-
+import photo from "../../../assets/images/Pricing/Buying/rafiki.png";
 const Buying = () => {
     return (
         <div className="mb-16 grid lg:grid-cols-2 grid-cols-1 lg:gap-0 md:gap-20 gap-24 items-center">
@@ -7,7 +7,7 @@ const Buying = () => {
                 <h2 className="lg:text-5xl md:text-4xl text-3xl font-bold text-[#FF3737]">Buy Now!!!</h2>
             </div>
             <div>
-                <img className="lg:w-auto md:w-[450px] w-[350px] lg:mx-0 mx-auto" src="https://i.ibb.co/w7YNtc7/rafiki.png" alt="" />
+                <img className="lg:w-auto md:w-[450px] w-[350px] lg:mx-0 mx-auto" src={photo} alt="photo" />
             </div>
         </div>
     );
