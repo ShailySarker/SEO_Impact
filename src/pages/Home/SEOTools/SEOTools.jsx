e22a5a1040c6eb2ed1c96a286b3bb356bebb4175
ShailySarker <shailysarker99@gmail.com>
2023-11-03 16:42:38 +0600

    home_SEOTools section updated

--- src/pages/Home/SEOTools/SEOTools.jsx
+++ src/pages/Home/SEOTools/SEOTools.jsx
@@ -1,3 +1,7 @@
+import clockImage from '../../../assets/images/Home/SEOTools/material-symbols_alarm.png';
+import uploadImage from '../../../assets/images/Home/SEOTools/streamline_interface-upload-desktop-action-actions-computer-desktop-device-display-monitor-screen-upload.png';
+import laptopImage from '../../../assets/images/Home/SEOTools/ion_laptop-sharp.png';
+
 
 const SEOTools = () => {
     return (
@@ -5,18 +9,16 @@ const SEOTools = () => {
             <h2 className="font-bold lg:text-5xl md:text-4xl text-3xl text-center mb-16 text-[#212121]">Unlock your success now with our <br /> SEO Tools </h2>
             <div className="lg:flex lg:flex-row grid md:grid-cols-2 grid-cols-1 lg:gap-24 lg:justify-center lg:mx-auto gap-16">
                 <div>
-                    <img className="flex mx-auto bg-[#EBF5FF] p-5 w-24 rounded-t-full relative -mb-10" src="https://i.ibb.co/6HxwJxW/material-symbols-alarm.png" alt="" />
+                    <img className="flex mx-auto bg-[#EBF5FF] p-5 w-24 rounded-t-full relative -mb-10" src={clockImage} alt="clockImage" />
                     <p className="font-bold text-xl bg-[#EBF5FF] py-12 px-28 rounded-3xl ">QUICK</p>
-                    {/* <img className="flex mx-auto " src="https://i.ibb.co/Bc4nCJ5/Rectangle-14.png" alt="" />  */}
                 </div>
                 <div>
-                    <img className="flex mx-auto bg-[#EBF5FF] p-5 w-24 rounded-t-full relative -mb-10" src="https://i.ibb.co/6YnCCXb/streamline-interface-upload-desktop-action-actions-computer-desktop-device-display-monitor-screen-up.png" alt="" />
+                    <img className="flex mx-auto bg-[#EBF5FF] p-5 w-24 rounded-t-full relative -mb-10" src={uploadImage} alt="uploadImage" />
                     <p className="font-bold text-xl bg-[#EBF5FF] py-12 px-24 rounded-3xl ">FOCUSSED</p>
                 </div>
                 <div>
-                    <img className="flex mx-auto bg-[#EBF5FF] p-5 w-24 rounded-t-full relative -mb-10" src="https://i.ibb.co/mNNz04B/ion-laptop-sharp.png" alt="" />
+                    <img className="flex mx-auto bg-[#EBF5FF] p-5 w-24 rounded-t-full relative -mb-10" src={laptopImage} alt="laptopImage" />
                     <p className="font-bold text-xl bg-[#EBF5FF] py-12 px-20 rounded-3xl ">ACTIONABLE</p>
-                    {/* <img className="flex mx-auto " src="https://i.ibb.co/Bc4nCJ5/Rectangle-14.png" alt="" />  */}
                 </div>
             </div>
         </div>
