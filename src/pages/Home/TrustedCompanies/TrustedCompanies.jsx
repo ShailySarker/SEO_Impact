137b423c7cb5593a4093cd61c234d88a9c1b3ddb
ShailySarker <shailysarker99@gmail.com>
2023-11-03 16:26:06 +0600

    home_trustedCompanies section updated

--- src/pages/Home/TrustedCompanies/TrustedCompanies.jsx
+++ src/pages/Home/TrustedCompanies/TrustedCompanies.jsx
@@ -1,13 +1,18 @@
+import company1 from '../../../assets/images/Home/TrustedCompanies/Apple_logo.png';
+import company2 from '../../../assets/images/Home/TrustedCompanies/Microsoft_logo.png';
+import company3 from '../../../assets/images/Home/TrustedCompanies/Samsung_logo.png';
+import company4 from '../../../assets/images/Home/TrustedCompanies/Nvidia_logo.png';
+
 
 const TrustedCompanies = () => {
     return (
         <div>
             <h2 className="text-center font-bold lg:text-5xl md:text-4xl text-3xl mb-6">Trusted By Many Companies</h2>
             <div className="mt-16 bg-[#EBF5FF] grid lg:grid-cols-4 grid-cols-2 md:py-16 p-10 md:px-28 md:gap-20 gap-10 items-center">
-                <img className="mx-auto h-10" src="https://i.ibb.co/tYQYFH9/Apple-logo.png" alt="" />
-                <img className="mx-auto h-10" src="https://i.ibb.co/tsvJhgP/Microsoft-logo.png" alt="" />
-                <img className="mx-auto" src="https://i.ibb.co/DWsC2GY/Samsung-logo.png" alt="" />
-                <img className="mx-auto h-8" src="https://i.ibb.co/B6H0pVv/Nvidia-logo.png" alt="" />
+                <img className="mx-auto h-10" src={company1} alt="company1" />
+                <img className="mx-auto h-10" src={company2} alt="company2" />
+                <img className="mx-auto" src={company3} alt="company3" />
+                <img className="mx-auto h-8" src={company4} alt="company4" />
             </div>
         </div>
     );
