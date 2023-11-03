a9727173bf78e5f78ab84ca6e676e74fd305c366
ShailySarker <shailysarker99@gmail.com>
2023-11-03 20:33:21 +0600

    home_features section updated

--- src/components/FeatureSectionSearchBar/FeatureSectionSearchBar.jsx
+++ src/components/FeatureSectionSearchBar/FeatureSectionSearchBar.jsx
@@ -1,10 +1,11 @@
-;
+import searchBar from "../../assets/images/Home/Features/search.png";
+import design from "../../assets/images/Home/Features/design.png";
 
 const FeatureSectionSearchBar = () => {
     return (
         <div className='text-white flex '>
-            <img className="w-56 h-44" src="https://i.ibb.co/6ymMbKV/Vector-4-1.png" alt="" />
-            <img className="w-28 h-28" src="https://i.ibb.co/CM6VMdQ/Group-5-1.png" alt="" />
+            <img className="w-56 h-44" src={design} alt="design" />
+            <img className="w-28 h-28" src={searchBar} alt="searchBar" />
         </div>
     );
 };
