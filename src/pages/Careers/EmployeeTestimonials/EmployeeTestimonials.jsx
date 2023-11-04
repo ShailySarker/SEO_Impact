5c3e6c8a5524788e126252d2d3046e91560a67e8
ShailySarker <shailysarker99@gmail.com>
2023-11-04 18:27:22 +0600

    careers_EmployeeTestimonials section updated with lazyLoaderImage

--- src/pages/Careers/EmployeeTestimonials/EmployeeTestimonials.jsx
+++ src/pages/Careers/EmployeeTestimonials/EmployeeTestimonials.jsx
@@ -1,3 +1,5 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import Employee from "../../../assets/images/Careers/EmployeeTestimonials/photo.png";
 
 const EmployeeTestimonials = () => {
     return (
@@ -7,21 +9,33 @@ const EmployeeTestimonials = () => {
                 <p className="lg:text-2xl md:text-xl text-lg text-[#757575] font-semibold lg:mx-36 text-center ">Lorem ipsum dolor sit amet consectetur. Quis eget vivamus neque velit enim id urna pharetra vel. Sapien ut natoque facilisi tortor viverra eget.</p>
             </div>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-16 gap-20 mt-20">
-                <div className="border-2 border-[#007BFE] rounded-xl lg:px-6 md:px-8 px-4 py-8 flex flex-col justify-center items-center hover:bg-[#EBF5FF]">
+                <div className="border-2 border-[#007BFE] rounded-xl lg:px-6 md:px-8 px-4 py-8 flex flex-col justify-center items-center hover:bg-[#EBF5FF] transition-transform duration-500 ease-in-out transform hover:scale-105">
                     <h2 className="lg:text-3xl md:text-2xl text-xl font-bold text-center pb-10">“<span className="text-[#007BFE]">SEO Impact</span> is a great place for working”</h2>
-                    <img className="w-48 " src="https://i.ibb.co/wR4F3cc/Ellipse-10.png" alt="" />
+                    <LazyLoadImage
+                        className="w-48 "
+                        src={Employee}
+                        alt="Employee Photo"
+                    />
                     <h3 className="font-bold pt-7 md:text-3xl text-2xl text-center">Rio Anna</h3>
                     <p className="pt-4 md:text-xl text-lg font-semibold text-center border-b-2 pb-6">UI/UX Designer, SEO Impact</p>
                 </div>
-                <div className="border-2 border-[#007BFE] rounded-xl lg:px-6 md:px-8 px-4 py-8 flex flex-col justify-center items-center hover:bg-[#EBF5FF]">
+                <div className="border-2 border-[#007BFE] rounded-xl lg:px-6 md:px-8 px-4 py-8 flex flex-col justify-center items-center hover:bg-[#EBF5FF] transition-transform duration-500 ease-in-out transform hover:scale-105">
                     <h2 className="lg:text-3xl md:text-2xl text-xl font-bold text-center pb-10">“<span className="text-[#007BFE]">SEO Impact</span> is a great place for developing skills”</h2>
-                    <img className="w-48 " src="https://i.ibb.co/wR4F3cc/Ellipse-10.png" alt="" />
+                    <LazyLoadImage
+                        className="w-48 "
+                        src={Employee}
+                        alt="Employee Photo"
+                    />
                     <h3 className="font-bold pt-7 md:text-3xl text-2xl text-center">Lucy Leo</h3>
                     <p className="pt-4 md:text-xl text-lg font-semibold text-center border-b-2 pb-6">Product Manager, SEO Impact</p>
                 </div>
-                <div className="border-2 border-[#007BFE] rounded-xl lg:px-6 md:px-8 px-4 py-8 flex flex-col justify-center items-center hover:bg-[#EBF5FF]">
+                <div className="border-2 border-[#007BFE] rounded-xl lg:px-6 md:px-8 px-4 py-8 flex flex-col justify-center items-center hover:bg-[#EBF5FF] transition-transform duration-500 ease-in-out transform hover:scale-105">
                     <h2 className="lg:text-3xl md:text-2xl text-xl font-bold text-center pb-10">“<span className="text-[#007BFE]">SEO Impact</span> is a great place for learning”</h2>
-                    <img className="w-48 " src="https://i.ibb.co/wR4F3cc/Ellipse-10.png" alt="" />
+                    <LazyLoadImage
+                        className="w-48 "
+                        src={Employee}
+                        alt="Employee Photo"
+                    />
                     <h3 className="font-bold pt-7 md:text-3xl text-2xl text-center">Anney Bee</h3>
                     <p className="pt-4 md:text-xl text-lg font-semibold text-center border-b-2 pb-6">Software Analyst, SEO Impact</p>
                 </div>
