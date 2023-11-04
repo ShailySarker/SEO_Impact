0f2ab03a26ec3ffe189bd702f9b63e1523a5b37c
ShailySarker <shailysarker99@gmail.com>
2023-11-04 20:27:19 +0600

    careers_SEOImpactRating section updated

--- src/pages/Careers/SEOImpactRating/SEOImpactRating.jsx
+++ src/pages/Careers/SEOImpactRating/SEOImpactRating.jsx
@@ -1,20 +1,23 @@
+import photo from "../../../assets/images/Careers/SEOImpactRating/photo.png";
+import star from "../../../assets/images/Careers/SEOImpactRating/Star.png";
+
 
 const SEOImpactRating = () => {
     return (
         <div className="flex lg:mx-48 md:mx-16 mx-10 lg:pt-8 mb-20">
             <div className="grid md:grid-cols-3 grid-cols-1 items-center bg-[#F1F8FF] p-12 lg:gap-0 md:gap-10 gap-16">
                 <div className="flex gap-3 md:mx-0 mx-auto">
-                    <img className="lg:w-auto w-6 " src="https://i.ibb.co/gz2B1b4/Star-10.png" alt="" />
-                    <img className="lg:w-auto w-6" src="https://i.ibb.co/gz2B1b4/Star-10.png" alt="" />
-                    <img className="lg:w-auto w-6" src="https://i.ibb.co/gz2B1b4/Star-10.png" alt="" />
-                    <img className="lg:w-auto w-6" src="https://i.ibb.co/gz2B1b4/Star-10.png" alt="" />
-                    <img className="lg:w-auto w-6" src="https://i.ibb.co/gz2B1b4/Star-10.png" alt="" />
+                    <img className="lg:w-auto w-6 " src={star} alt="" />
+                    <img className="lg:w-auto w-6" src={star} alt="" />
+                    <img className="lg:w-auto w-6" src={star} alt="" />
+                    <img className="lg:w-auto w-6" src={star} alt="" />
+                    <img className="lg:w-auto w-6" src={star} alt="" />
                 </div>
                 <div>
                     <h4 className="font-bold text-3xl lg:text-start text-center">SEO Impact</h4>
                 </div>
                 <div>
-                    <img src="https://i.ibb.co/0cJGmhh/Frame-10.png" alt="" />
+                    <img src={photo} alt="" />
                 </div>
             </div>
         </div>
