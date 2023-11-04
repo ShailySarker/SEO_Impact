25423c2b23382233faa5e671e8ed6bc24c6e037e
ShailySarker <shailysarker99@gmail.com>
2023-11-05 00:13:24 +0600

    rankingChecker page updated

--- src/pages/Resources/RankingChecker/RankingChecker/RankingChecker.jsx
+++ src/pages/Resources/RankingChecker/RankingChecker/RankingChecker.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import SearchBar from "../../../../components/SearchBar/SearchBar";
+import design from '../../../../assets/images/Resources/RankingChecker/RankingChecker/design.png';
+
 
 const RankingChecker = () => {
     return (
@@ -7,7 +9,7 @@ const RankingChecker = () => {
             <SearchBar></SearchBar>
             <div className="grid md:grid-cols-2 grid-cols-1 justify-between items-center gap-5 mt-16">
                 <h2 className="font-bold lg:text-5xl md:text-4xl text-3xl lg:mr-24 mr-12"><span className="text-[#007BFE]">Experience</span> seamless elegance on the go </h2>
-                <img className="" src="https://i.ibb.co/Npg5LBV/Group-187.png" alt="" />
+                <img className="" src={design} alt="design" />
             </div>
             <div className="lg:mt-36 md:mt-28 mt-24 flex flex-col items-center md:gap-16 gap-12">
                 <input className="border-2 py-3 px-7 lg:w-80 md:w-96 w-72 rounded-lg" type="search" name="" id="" placeholder="Enter your website...." />
