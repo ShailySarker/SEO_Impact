5cff5065640fd6e43586821c1b4e1a5ad911122c
ShailySarker <shailysarker99@gmail.com>
2023-11-10 22:46:03 +0600

    Adding dynamic data in SocialMedia section

--- src/pages/ContactUs/Components/SocialMedia.jsx
+++ src/pages/ContactUs/Components/SocialMedia.jsx
@@ -7,6 +7,68 @@ import photo6 from '../../../assets/images/ContactUs/SocialMedia_whatsappLogo.pn
 import photo7 from '../../../assets/images/ContactUs/SocialMedia_messengerLogo.png';
 import photo8 from '../../../assets/images/ContactUs/SocialMedia_threadsLogo.png';
 
+const data = [
+    {
+        id: 1,
+        image: photo1,
+        tag: "Follow Our",
+        socialMedia: "Facebook Page"
+    },
+    {
+        id: 2,
+        image: photo2,
+        tag: "Follow Our",
+        socialMedia: "Instagram Page"
+    },
+    {
+        id: 3,
+        image: photo3,
+        tag: "Follow Our",
+        socialMedia: "LinkedIn Profile"
+    },
+    {
+        id: 4,
+        image: photo4,
+        tag: "Join Our",
+        socialMedia: "YouTube Channel"
+    },
+    {
+        id: 5,
+        image: photo5,
+        tag: "Follow Our",
+        socialMedia: "Twitter Profile"
+    },
+    {
+        id: 6,
+        image: photo6,
+        tag: "Join Our",
+        socialMedia: "WhatsApp Group"
+    },
+    {
+        id: 7,
+        image: photo7,
+        tag: "Join Our",
+        socialMedia: "Messenger Group"
+    },
+    {
+        id: 8,
+        image: photo8,
+        tag: "Join Our",
+        socialMedia: "Threads Group"
+    }
+];
+
+const SocialMediaInfo = ({ item }) => {
+    const { id, image, tag, socialMedia } = item;
+
+    return (
+        <div className="border-2 px-2 lg:py-8 md:py-5 py-4 rounded-xl border-[#007BFE] hover:shadow-2xl hover:shadow-blue-200">
+            <img className="lg:w-24 lg:h-24 md:w-20 md:h-20 w-16 h-16 mx-auto lg:mb-12 md:mb-8 mb-5" src={image} alt={`SocialMedial_${id} Photo`} />
+            <h3 className="text-center font-semibold lg:text-2xl text-xl md:pb-4 pb-2">{tag}</h3>
+            <h2 className="text-[#007BFE] font-bold text-center lg:text-2xl text-xl pb-2">{socialMedia}</h2>
+        </div>
+    );
+};
 
 const SocialMedia = () => {
     return (
@@ -16,46 +78,14 @@ const SocialMedia = () => {
                 <p className="lg:text-2xl md:text-xl text-lg text-[#757575] font-semibold lg:mx-36 text-center ">Lorem ipsum dolor sit amet consectetur. Quis eget vivamus neque velit enim id urna pharetra vel. Sapien ut natoque facilisi tortor viverra eget. Amet rhoncus leo sed nulla est senectus morbi tellus ornare.</p>
             </div>
             <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 lg:gap-12 md:gap-8 gap-6 justify-center items-center">
-                <div className="border-2 px-2 lg:py-8 md:py-5 py-4 rounded-xl border-[#007BFE] hover:shadow-2xl hover:shadow-blue-200">
-                    <img className="lg:w-24 lg:h-24 md:w-20 md:h-20 w-16 h-16 mx-auto lg:mb-12 md:mb-8 mb-5" src={photo1} alt="Facebook" />
-                    <h3 className="text-center font-semibold lg:text-2xl text-xl md:pb-4 pb-2">Follow Our </h3>
-                    <h2 className="text-[#007BFE] font-bold text-center lg:text-2xl text-xl pb-2">Facebook Page</h2>
-                </div>
-                <div className="border-2 px-2 lg:py-8 md:py-5 py-4 rounded-xl border-[#007BFE] hover:shadow-2xl hover:shadow-blue-200">
-                    <img className="lg:w-24 lg:h-24 md:w-20 md:h-20 w-16 h-16 mx-auto lg:mb-12 md:mb-8 mb-5" src={photo2} alt="Instagram" />
-                    <h3 className="text-center font-semibold lg:text-2xl text-xl md:pb-4 pb-2">Follow Our </h3>
-                    <h2 className="text-[#007BFE] font-bold text-center lg:text-2xl text-xl pb-2">Instagram Page</h2>
-                </div>
-                <div className="border-2 px-2 lg:py-8 md:py-5 py-4 rounded-xl border-[#007BFE] hover:shadow-2xl hover:shadow-blue-200">
-                    <img className="lg:w-24 lg:h-24 md:w-20 md:h-20 w-16 h-16 mx-auto lg:mb-12 md:mb-8 mb-5" src={photo3} alt="LinkedIn" />
-                    <h3 className="text-center font-semibold md:text-2xl text-xl md:pb-4 pb-2">Follow Our </h3>
-                    <h2 className="text-[#007BFE] font-bold text-center lg:text-2xl text-xl pb-2">LinkedIn Profile</h2>
-                </div>
-                <div className="border-2 px-2 lg:py-8 md:py-5 py-4 rounded-xl border-[#007BFE] hover:shadow-2xl hover:shadow-blue-200">
-                    <img className="lg:w-24 lg:h-24 md:w-20 md:h-20 w-16 h-16 mx-auto lg:mb-12 md:mb-8 mb-5" src={photo4} alt="YouTube" />
-                    <h3 className="text-center font-semibold md:text-2xl text-xl md:pb-4 pb-2">Join Our </h3>
-                    <h2 className="text-[#007BFE] font-bold text-center lg:text-2xl text-xl pb-2">YouTube Channel</h2>
-                </div>
-                <div className="border-2 px-2 lg:py-8 md:py-5 py-4 rounded-xl border-[#007BFE] hover:shadow-2xl hover:shadow-blue-200">
-                    <img className="lg:w-24 lg:h-24 md:w-20 md:h-20 w-16 h-16 mx-auto lg:mb-12 md:mb-8 mb-5" src={photo5} alt="Twitter" />
-                    <h3 className="text-center font-semibold md:text-2xl text-xl md:pb-4 pb-2">Follow Our </h3>
-                    <h2 className="text-[#007BFE] font-bold text-center lg:text-2xl text-xl pb-2">Twitter Profile</h2>
-                </div>
-                <div className="border-2 px-2 lg:py-8 md:py-5 py-4 rounded-xl border-[#007BFE] hover:shadow-2xl hover:shadow-blue-200">
-                    <img className="lg:w-24 lg:h-24 md:w-20 md:h-20 w-16 h-16 mx-auto lg:mb-12 md:mb-8 mb-5" src={photo6} alt="WhatsApp" />
-                    <h3 className="text-center font-semibold md:text-2xl text-xl md:pb-4 pb-2">Join Our </h3>
-                    <h2 className="text-[#007BFE] font-bold text-center lg:text-2xl text-xl pb-2">WhatsApp Group</h2>
-                </div>
-                <div className="border-2 px-2 lg:py-8 md:py-5 py-4 rounded-xl border-[#007BFE] hover:shadow-2xl hover:shadow-blue-200">
-                    <img className="lg:w-24 lg:h-24 md:w-20 md:h-20 w-16 h-16 mx-auto lg:mb-12 md:mb-8 mb-5" src={photo7} alt="Messenger" />
-                    <h3 className="text-center font-semibold md:text-2xl text-xl md:pb-4 pb-2">Join Our </h3>
-                    <h2 className="text-[#007BFE] font-bold text-center lg:text-2xl text-xl pb-2">Messenger Group</h2>
-                </div>
-                <div className="border-2 px-2 lg:py-8 md:py-5 py-4 rounded-xl border-[#007BFE] hover:shadow-2xl hover:shadow-blue-200">
-                    <img className="lg:w-24 lg:h-24 md:w-20 md:h-20 w-16 h-16 mx-auto lg:mb-12 md:mb-8 mb-5" src={photo8} alt="Threads" />
-                    <h3 className="text-center font-semibold md:text-2xl text-xl md:pb-4 pb-2">Join Our </h3>
-                    <h2 className="text-[#007BFE] font-bold text-center lg:text-2xl text-xl pb-2">Threads Group</h2>
-                </div>
+                {
+                    data.map((item) => (
+                        <SocialMediaInfo
+                            item={item}
+                            key={item.id}
+                        />
+                    ))
+                }
             </div>
         </div>
     );
