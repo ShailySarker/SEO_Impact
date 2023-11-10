f7194c0cb411274eded27fb83a3960f48dd0c83e
ShailySarker <shailysarker99@gmail.com>
2023-11-10 19:40:11 +0600

    Adding dynamic data in TrainingAndDevelopment section

--- src/pages/Careers/Components/TrainingAndDevelopment.jsx
+++ src/pages/Careers/Components/TrainingAndDevelopment.jsx
@@ -9,6 +9,55 @@ import program4 from "../../../assets/images/Careers/TrainingAndDevelopment_prog
 import program5 from "../../../assets/images/Careers/TrainingAndDevelopment_program5.jpg";
 import program6 from "../../../assets/images/Careers/TrainingAndDevelopment_program6.jpg";
 
+const data = [
+    {
+        id: 1,
+        image: program1,
+        title: "Orientation Programs",
+        description: "These programs are for new hires and are designed to introduce them to the company’s culture, policies, and procedures."
+    },
+    {
+        id: 2,
+        image: program2,
+        title: "Technical Skills Workshops",
+        description: "These workshops concentrate on improving specific technical skills, such as coding, data analysis, or software proficiency."
+    },
+    {
+        id: 3,
+        image: program3,
+        title: "Leadership Development",
+        description: "Geared towards grooming future leaders, these programs focus on enhancing leadership skills, decision-making, and strategic thinking."
+    },
+    {
+        id: 4,
+        image: program4,
+        title: "Customer Service Training",
+        description: "For customer-facing roles, this training helps employees develop excellent communication and problem-solving skills."
+    },
+    {
+        id: 5,
+        image: program5,
+        title: "Soft Skills Training",
+        description: "Focusing on emotional intelligence, time management, stress management, and teamwork, these programs improve personal effectiveness."
+    },
+    {
+        id: 6,
+        image: program6,
+        title: "Wellness Programs",
+        description: "Designed to promote employee well-being, they include yoga classes, mindfulness sessions, and stress management workshops."
+    }
+];
+
+const TrainingAndDevelopmentInfo = ({ item }) => {
+    const { id, image, title, description } = item;
+    return (
+        <div className='border-2 rounded-xl border-[#007BFE] md:p-6 p-4 lg:h-[580px] md:h-[540px] h-[450px] mb-12'>
+            <img className='rounded-xl lg:h-[270px] w-full md:h-[230px] h-52' src={image} alt={`Program_${id} Photo`} />
+            <h3 className='font-semibold lg:text-3xl md:text-2xl text-xl lg:pt-12 md:pt-9 pt-7 lg:pb-7 md:pb-6 pb-4 text-center text-black'>{title}</h3>
+            <p className='md:text-lg font-medium lg:pb-6 md:pb-5 pb-4'>{description}</p>
+        </div>
+    );
+};
 
 const TrainingAndDevelopment = () => {
 
@@ -41,7 +90,6 @@ const TrainingAndDevelopment = () => {
         ]
     };
 
-
     return (
         <div className="lg:pt-8 md:pt-5 pt-0 lg:mb-36 lg:mx-16 md:mb-0 mb-9 md:mx-16 mx-6">
             <div className='lg:mb-28 md:mb-20 mb-2'>
@@ -50,36 +98,14 @@ const TrainingAndDevelopment = () => {
             </div>
             <div className="mt-20 mb-28">
                 <Slider {...settings}>
-                    <div className='border-2 rounded-xl border-[#007BFE] md:p-6 p-4 lg:h-[580px] md:h-[540px] h-[450px] mb-12'>
-                        <img className='rounded-xl lg:h-[270px] w-full md:h-[230px] h-52' src={program1} alt="Program1 Photo" />
-                        <h3 className='font-semibold lg:text-3xl md:text-2xl text-xl lg:pt-12 md:pt-9 pt-7 lg:pb-7 md:pb-6 pb-4 text-center text-black'>Orientation Programs</h3>
-                        <p className='md:text-lg font-medium lg:pb-6 md:pb-5 pb-4'> These programs are for new hires and are designed to introduce them to the company’s culture, policies, and procedures.</p>
-                    </div>
-                    <div className='border-2 rounded-xl border-[#007BFE] md:p-6 p-4 lg:h-[580px] md:h-[540px] h-[450px] mb-12'>
-                    <img className='rounded-xl lg:h-[270px] w-full md:h-[230px] h-52' src={program2} alt="Program2 Photo" />
-                        <h3 className='font-semibold lg:text-3xl md:text-2xl text-xl lg:pt-12 md:pt-9 pt-7 lg:pb-7 md:pb-6 pb-4 text-center text-black'>Technical Skills Workshops</h3>
-                        <p className='md:text-lg font-medium lg:pb-6 md:pb-5 pb-4'> These workshops concentrate on improving specific technical skills, such as coding, data analysis, or software proficiency.</p>
-                    </div>
-                    <div className='border-2 rounded-xl border-[#007BFE] md:p-6 p-4 lg:h-[580px] md:h-[540px] h-[450px] mb-12'>
-                    <img className='rounded-xl lg:h-[270px] w-full md:h-[230px] h-52' src={program3} alt="Program3 Photo" />
-                        <h3 className='font-semibold lg:text-3xl md:text-2xl text-xl lg:pt-12 md:pt-9 pt-7 lg:pb-7 md:pb-6 pb-4 text-center text-black'>Leadership Development</h3>
-                        <p className='md:text-lg font-medium lg:pb-6 md:pb-5 pb-4'>Geared towards grooming future leaders, these programs focus on enhancing leadership skills, decision-making, and strategic thinking.</p>
-                    </div>
-                    <div className='border-2 rounded-xl border-[#007BFE] md:p-6 p-4 lg:h-[580px] md:h-[540px] h-[450px] mb-12'>
-                    <img className='rounded-xl lg:h-[270px] w-full md:h-[230px] h-52' src={program4} alt="Program4 Photo" />
-                        <h3 className='font-semibold lg:text-3xl md:text-2xl text-xl lg:pt-12 md:pt-9 pt-7 lg:pb-7 md:pb-6 pb-4 text-center text-black'>Customer Service Training</h3>
-                        <p className='md:text-lg font-medium lg:pb-6 md:pb-5 pb-4'>For customer-facing roles, this training helps employees develop excellent communication and problem-solving skills.</p>
-                    </div>
-                    <div className='border-2 rounded-xl border-[#007BFE] md:p-6 p-4 lg:h-[580px] md:h-[540px] h-[450px] mb-12'>
-                    <img className='rounded-xl lg:h-[270px] w-full md:h-[230px] h-52' src={program5} alt="Program5 Photo" />
-                        <h3 className='font-semibold lg:text-3xl md:text-2xl text-xl lg:pt-12 md:pt-9 pt-7 lg:pb-7 md:pb-6 pb-4 text-center text-black'>Soft Skills Training</h3>
-                        <p className='md:text-lg font-medium lg:pb-6 md:pb-5 pb-4'>Focusing on emotional intelligence, time management, stress management, and teamwork, these programs improve personal effectiveness.</p>
-                    </div>
-                    <div className='border-2 rounded-xl border-[#007BFE] md:p-6 p-4 lg:h-[580px] md:h-[540px] h-[450px] mb-12'>
-                    <img className='rounded-xl lg:h-[270px] w-full md:h-[230px] h-52' src={program6} alt="Program6 Photo" />
-                        <h3 className='font-semibold lg:text-3xl md:text-2xl text-xl lg:pt-12 md:pt-9 pt-7 lg:pb-7 md:pb-6 pb-4 text-center text-black'>Wellness Programs</h3>
-                        <p className='md:text-lg font-medium lg:pb-6 md:pb-5 pb-4'>Designed to promote employee well-being, they include yoga classes, mindfulness sessions, and stress management workshops.</p>
-                    </div>
+                    {
+                        data.map((item) => (
+                            <TrainingAndDevelopmentInfo
+                                item={item}
+                                key={item.id}
+                            />
+                        ))
+                    }
                 </Slider>
             </div >
         </div >
