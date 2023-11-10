3d4e8eff30bb43ce9a1704ac6ac6ebdf3fc5a3cf
ShailySarker <shailysarker99@gmail.com>
2023-11-10 17:21:10 +0600

    Adding dynamic data in applicationProcess section

--- src/pages/Careers/Components/ApplicationProcess.jsx
+++ src/pages/Careers/Components/ApplicationProcess.jsx
@@ -1,3 +1,38 @@
+const data = [
+    {
+        id: 1,
+        stepNo: "Step_01",
+        title: "Explore Our Job and Internship Lists"
+    },
+    {
+        id: 2,
+        stepNo: "Step_02",
+        title: "Read All Requirements Carefully"
+    },
+    {
+        id: 3,
+        stepNo: "Step_03",
+        title: "Choose Your Suitable Job/Internship"
+    },
+    {
+        id: 4,
+        stepNo: "Step_04",
+        title: "Complete the Applying Process"
+    }
+];
+
+const ApplicationProcessInfo = ({ item }) => {
+    return (
+        <div className="flex items-center lg:justify-center lg:gap-8 md:gap-12 gap-7 ">
+            <div>
+                <h2 className="md:text-3xl text-xl font-semibold text-[#007BFE]">{item.stepNo}:</h2>
+            </div>
+            <div className="bg-[#EBF5FF] hover:bg-[#007BFE] hover:text-white rounded-xl flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE] w-full transform transition-transform duration-700 ease-in-out">
+                <h3 className="md:text-3xl text-xl font-semibold p-5 ">{item.title}</h3>
+            </div>
+        </div>
+    );
+};
 
 const ApplicationProcess = () => {
     return (
@@ -7,38 +42,14 @@ const ApplicationProcess = () => {
                 <p className="lg:text-2xl md:text-xl text-lg text-[#757575] font-semibold lg:mx-36 text-center ">Lorem ipsum dolor sit amet consectetur. Quis eget vivamus neque velit enim id urna pharetra vel. Sapien ut natoque facilisi tortor viverra eget. Amet rhoncus leo sed nulla est senectus morbi tellus ornare.</p>
             </div>
             <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-20 gap-12 lg:mx-32 md:mx-12">
-                <div className="flex items-center lg:justify-center lg:gap-8 md:gap-12 gap-7 ">
-                    <div>
-                        <h2 className="md:text-3xl text-xl font-semibold text-[#007BFE]">Step_01:</h2>
-                    </div>
-                    <div className="bg-[#EBF5FF] hover:bg-[#007BFE] hover:text-white rounded-xl flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE] w-full transform transition-transform duration-700 ease-in-out">
-                        <h3 className="md:text-3xl text-xl font-semibold p-5 ">Explore Our Job and Internship Lists</h3>
-                    </div>
-                </div>
-                <div className="flex items-center lg:justify-center lg:gap-8 md:gap-12 gap-7 ">
-                    <div>
-                        <h2 className="md:text-3xl text-xl font-semibold text-[#007BFE]">Step_02:</h2>
-                    </div>
-                    <div className="bg-[#EBF5FF] hover:bg-[#007BFE] hover:text-white rounded-xl flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE] w-full transform transition-transform duration-700 ease-in-out">
-                        <h3 className="md:text-3xl text-xl font-semibold p-5 ">Read All Requirements Carefully</h3>
-                    </div>
-                </div>
-                <div className="flex items-center lg:justify-center lg:gap-8 md:gap-12 gap-7 ">
-                    <div>
-                        <h2 className="md:text-3xl text-xl font-semibold text-[#007BFE]">Step_03:</h2>
-                    </div>
-                    <div className="bg-[#EBF5FF] hover:bg-[#007BFE] hover:text-white rounded-xl flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE] w-full transform transition-transform duration-700 ease-in-out">
-                        <h3 className="md:text-3xl text-xl font-semibold p-5 ">Choose Your Suitable Job/Internship</h3>
-                    </div>
-                </div>
-                <div className="flex items-center lg:justify-center lg:gap-8 md:gap-12 gap-7 ">
-                    <div>
-                        <h2 className="md:text-3xl text-xl font-semibold text-[#007BFE]">Step_04:</h2>
-                    </div>
-                    <div className="bg-[#EBF5FF] hover:bg-[#007BFE] hover:text-white rounded-xl flex justify-center items-center shadow-lg text-center border-2 border-[#007BFE] w-full transform transition-transform duration-700 ease-in-out">
-                        <h3 className="md:text-3xl text-xl font-semibold p-5 ">Complete the Applying Process</h3>
-                    </div>
-                </div>
+                {
+                    data.map((item) => (
+                        <ApplicationProcessInfo
+                            item={item}
+                            key={item.id}
+                        />
+                    ))
+                }
             </div>
         </div >
     );
