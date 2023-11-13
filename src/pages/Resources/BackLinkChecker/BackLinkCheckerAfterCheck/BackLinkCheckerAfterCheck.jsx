fb2e858dc3f6395ef3bfe099e669d0241525dc37
ShailySarker <shailysarker99@gmail.com>
2023-11-13 18:59:38 +0600

    Updating backLinkCheckAfterCheck_page assets folder structure and also adding light weight images

--- src/pages/Resources/BackLinkChecker/BackLinkCheckerAfterCheck/BackLinkCheckerAfterCheck.jsx
+++ src/pages/Resources/BackLinkChecker/BackLinkCheckerAfterCheck/BackLinkCheckerAfterCheck.jsx
@@ -1,17 +1,28 @@
-import BackLinkResultDescription from "./Components/BackLinkResultDescription";
-import BackLinkResultOverview from "./Components/BackLinkResultOverview";
+// import BackLinkHeader from "./Components/BackLinkHeader";
+// import BackLinkResultDescription from "./Components/BackLinkResultDescription";
+
+import React, { Suspense } from "react";
+import ComponentLazyLoaderDesign from "../../../../components/ComponentLazyLoaderDesign/ComponentLazyLoaderDesign";
+
+// import BackLinkResultOverview from "./Components/BackLinkResultOverview";
+const BackLinkHeader = React.lazy(() => import("./Components/BackLinkHeader"));
+const BackLinkResultDescription = React.lazy(() => import("./Components/BackLinkResultDescription"));
+const BackLinkResultOverview = React.lazy(() => import("./Components/BackLinkResultOverview"));
 
 const BackLinkCheckerAfterCheck = () => {
     return (
         <div>
-            <div className="mt-12 lg:ml-16 md:ml-14 md:mr-0 mx-6">
-                <h2 className="font-semibold lg:text-start text-center lg:text-5xl md:text-4xl text-3xl">Backlink Checker</h2>
-                <div className="mt-24 flex flex-col items-center gap-10">
-                    <input className="border-2 py-3 px-7 md:w-96 w-72 rounded-lg" type="search" name="" id="" placeholder="cbb.coastalbroadband.in" disabled />
-                </div>
-            </div>
-            <BackLinkResultOverview></BackLinkResultOverview>
-            <BackLinkResultDescription></BackLinkResultDescription>
+            <Suspense fallback={<ComponentLazyLoaderDesign />}>
+                <BackLinkHeader />
+            </Suspense>
+            <Suspense fallback={<ComponentLazyLoaderDesign />}>
+                <BackLinkResultOverview />
+            </Suspense>
+            <Suspense fallback={<ComponentLazyLoaderDesign />}>
+                <BackLinkResultDescription />
+            </Suspense>
+            {/* <BackLinkHeader></BackLinkHeader>            <BackLinkResultOverview></BackLinkResultOverview>
+            <BackLinkResultDescription></BackLinkResultDescription> */}
         </div>
     );
 };
---
+++ src/pages/Resources/BackLinkChecker/BackLinkCheckerAfterCheck/Components/BackLinkHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BackLinkHeader = () => {
+    return (
+        <div>
+            <div className="mt-12 lg:ml-16 md:ml-14 md:mr-0 mx-6">
+                <h2 className="font-semibold lg:text-start text-center lg:text-5xl md:text-4xl text-3xl">Backlink Checker</h2>
+                <div className="mt-24 flex flex-col items-center gap-10">
+                    <input className="border-2 py-3 px-7 md:w-96 w-72 rounded-lg" type="search" name="" id="" placeholder="cbb.coastalbroadband.in" disabled />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BackLinkHeader;
